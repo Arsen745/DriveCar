4579b37eb9fa014259201c6718289fa5675dc0d6
Arsen <arsenkerezbekov7@gmail.com>
2024-09-05 00:32:40 +0600

    cards

--- src/component/section/Section.jsx
+++ src/component/section/Section.jsx
@@ -1,7 +1,69 @@
+import './Section.css'
+import Logo from '../../assets/Logo.png'
+import Vector from '../../assets/Vector.png'
 
 const Section = () => {
   return (
-    <div>Section</div>
+    <div style={{backgroundImage: `url(${Logo})`}} className="container-section">
+      <div className="cards">
+        <div className="text">
+          <h1>Наши услуги</h1>
+        </div>
+        <div className="card-content">
+
+        <div className="card">
+          <div className="icon">
+            <img src={Vector} alt="" />
+          </div>
+          <div className="text-card">
+            <h2>Оценка автомобиля</h2>
+          </div>
+        </div>
+        <div className="card">
+          <div className="icon">
+            <img src={Vector} alt="" />
+          </div>
+          <div className="text-card">
+            <h2>Подготова к продаже </h2>
+          </div>
+        </div>
+        <div className="card">
+          <div className="icon">
+            <img src={Vector} alt="" />
+          </div>
+          <div className="text-card">
+            <h2>Комиссионная продажа</h2>
+          </div>
+        </div>
+        <div className="card">
+          <div className="icon">
+            <img src={Vector} alt="" />
+          </div>
+          <div className="text-card">
+            <h2>Консультатция по рынку</h2>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="icon">
+            <img src={Vector} alt="" />
+          </div>
+          <div className="text-card">
+            <h2>-Обмен старого автомобиля на новый с доплатой</h2>
+          </div>
+        </div>
+        <div className="card">
+          <div className="icon">
+            <img src={Vector} alt="" />
+          </div>
+          <div className="text-card">
+            <h2>Фотосъемка и создание объявления</h2>
+          </div>
+        </div> 
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
